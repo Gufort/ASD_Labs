dda4cf0d844d346a3319a9895f72691112655399
Gufort <guffy911d@gmail.com>
2025-04-20 17:18:19 +0300

    Some tries to create CustomHashSet

--- Lab_07/src/CustomHashSet.java
+++ Lab_07/src/CustomHashSet.java
@@ -10,29 +10,33 @@ public class CustomHashSet<T> {
     private Function<T, Integer> _hashFuction;
     private Object[] _elements;
 
-   public CustomHashSet(int capacity, Function<T, Integer> hashFuction, Comparator<T> compare) {
-       _capacity = capacity;
-       _hashFuction = hashFuction;
-       _compare = compare;
-       _size = 0;
-       _countOfCollisions = 0;
-       _elements = new Object[_capacity];
+    public CustomHashSet(int capacity, Function<T, Integer> hashFuction, Comparator<T> compare) {
+        _capacity = capacity;
+        _hashFuction = hashFuction;
+        _compare = compare;
+        _size = 0;
+        _countOfCollisions = 0;
+        _elements = new Object[_capacity];
 
-       //Все клетки пустые
-       _states = new char[_capacity];
-       for(int i = 0; i < capacity; i++)
-           _states[i] = '0';
-   }
+        //Все клетки пустые
+        _states = new char[_capacity];
+        for(int i = 0; i < capacity; i++)
+            _states[i] = '0';
+    }
 
-   public CustomHashSet(int capacity) {
-       this(capacity,
-               key -> (key == null) ? 0 : key.hashCode(),
-               (a, b) -> a.equals(b) ? 0 : 1);
-   }
+    public CustomHashSet(int capacity) {
+        this(capacity,
+                key -> (key == null) ? 0 : key.hashCode(),
+                (a, b) -> a.equals(b) ? 0 : 1);
+    }
 
-   private int hash(T element) {
-       return Math.abs(_hashFuction.apply(element)) % _capacity;
-   }
+    private int hash(T element) {
+        return Math.abs(_hashFuction.apply(element)) % _capacity;
+    }
+
+    public int size() { return _size; }
+    public int capacity() { return _capacity; }
+    public int countOfCollisions() { return _countOfCollisions; }
 
     public boolean insert(T element) {
         if (_size >= _capacity)
@@ -58,5 +62,40 @@ public class CustomHashSet<T> {
             position = (position + 1) % _capacity;
         }
         return false;
+    }
+
+    public void erase(T element) {
+        int position = hash(element);
+        int start = position;
+
+        while(position == start) {
+            if(_states[position] == '1' &&
+            _compare.compare((T)_elements[position], element) == 0) {
+                _elements[position] = null;
+                _states[position] = '2';
+                _size--;
+            }
+            position = (position + 1) % _capacity;
+        }
+    }
+
+
+
+    public void print(){
+        System.out.println("Hash table:");
+        System.out.printf("%-10s %-15s %-15s %s%n", "Value", "Hash", "Key", "State");
+
+        for(int i = 0; i < _capacity; i++){
+            String s = (_states[i] == '1') ? "Занято" :
+                       (_states[i] == '0') ? "Свободно" :
+                       "Удалено";
+
+            System.out.printf("%-10d %-15s %-15s %s%n",
+                    i,
+                    (_states[i] == '0') ? "empty" : hash((T)_elements[i]),
+                    (_states[i] == '0' || _states[i] == '2') ? "empty" :
+                            (_elements[i] != null) ? _elements[i].toString() : "null",
+                    s);
+        }
     }
 }
--- Lab_07/src/Main.java
+++ Lab_07/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         CustomHashSet<Integer> set = new CustomHashSet<>(10);
-        set.insert(1);
+
+        Random rand = new Random();
+
+        for(int i = 0; i < 4; i++){
+            set.insert(3123123 + i*1234);
+        }
+
+        System.out.println(set.capacity());
+        set.print();
+
+        set.erase(3124357);
+        set.print();
     }
 }
